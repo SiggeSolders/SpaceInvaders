bdda2a9bcd97f9f3aa225f46f2293fb68aed258e
SiggeSolders <sigge.hallquistsolders@elev.ga.lbs.se>
2024-10-10 17:45:48 +0200

    Avslutade Equalizern

--- Assets/Skript/Visualizer.cs
+++ Assets/Skript/Visualizer.cs
@@ -5,23 +5,47 @@ using UnityEngine.UI;
 
 public class Visualizer : MonoBehaviour
 {
-    public Color visualizerColor = Color.gray;
+    public float minHeight = 15.0f;
+    public float maxHeight = 425.0f;
+    public float updateSenistivity = 0.5f;
     
-    public VisualizerObjectSkript[] visualizerObjects;
+    public Color visualizerColor = Color.gray;
 
     [Space(15)]
     public AudioClip audioClip;
     public bool loop = true;
+    [Space(15), Range(64, 8192)]
+    public int visualzierSimples = 64;
+
+    public VisualizerObjectSkript[] visualizerObjects;
+    AudioSource m_audioSource;
+
     void Start()
     {
         visualizerObjects = GetComponentsInChildren<VisualizerObjectSkript>();
+
+        if (!audioClip)
+        {
+            return;
+        }
+        m_audioSource = new GameObject("AudioSource").AddComponent<AudioSource>();
+        m_audioSource.loop = loop;
+        m_audioSource.clip = audioClip;
+        m_audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float[] spectrumData = m_audioSource.GetSpectrumData(visualzierSimples, 0, FFTWindow.Rectangular);
+        
         for(int i = 0; i < visualizerObjects.Length; i++)
         {
+            Vector2 newSize = visualizerObjects[i].GetComponent<RectTransform>().rect.size;
+
+            newSize.y = Mathf.Lerp (newSize.y, minHeight +(spectrumData[i] * (maxHeight - minHeight) * 5.0f), updateSenistivity);
+            visualizerObjects[i].GetComponent<RectTransform>().sizeDelta = newSize;
+            
             visualizerObjects[i].GetComponent<Image>().color = visualizerColor;
         }
     }

8770d5be6018f7c6a245462d6a47666964f3e2bc
CharlieSU23B <charlie.buhrehagglund@elev.ga.lbs.se>
2024-10-15 12:12:30 +0200

    Added beat counting and shooting on the beat

---
+++ Assets/Skript/Conductor/BeatCounter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using System;
+
+public class BeatCounter : MonoBehaviour
+{
+    private float songPos;
+    private double songPosRoundedUp;
+    private double songPosRoundedDown;
+
+    public bool inSync;
+    public bool missleShot;
+
+    //int roundedUpValue;
+
+    public GameObject conductor;
+    ConductorScript beatConductor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //roundedUpValue = 1;
+        beatConductor = conductor.GetComponent<ConductorScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //uppdaterar låt positionen
+        songPos = beatConductor.songPositionInBeats;
+        songPosRoundedUp = Math.Ceiling(songPos);
+        songPosRoundedDown = Math.Floor(songPos);
+
+        //kollar om låten är vid en beat
+        if(songPosRoundedUp - songPos <= 0.15 && songPosRoundedUp - songPos >= 0)
+        {
+            inSync = true;
+        }
+        else if(songPosRoundedDown - songPos >= 0.15 && songPosRoundedDown - songPos <= 0)
+        {
+            inSync = true;
+        }
+        else
+        {
+            inSync = false;
+        }
+    }
+}

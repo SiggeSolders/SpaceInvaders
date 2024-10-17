997acfe138bdc70e3f4a0656d809c7f28e7ad101
CharlieSU23B <charlie.buhrehagglund@elev.ga.lbs.se>
2024-10-17 15:54:09 +0200

    La till edge och fick allt att röra sig som de ska

---
+++ Assets/Skript/SkriptKopior/MysterShipSkriptKopia.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MysteryShipSkriptKopia : MonoBehaviour
+{
+    float speed = 5f;
+    float cycleTime = 5f;
+
+    Vector2 leftDestination;
+    Vector2 rightDestination;
+    int direction = -1;
+    bool isVisible;
+
+    public GameObject lineRendererTarget;
+    LineRenderer lineR;
+
+
+    void Start()
+    {
+        lineR = lineRendererTarget.GetComponent<LineRenderer>();
+
+        Vector3 leftEdge = lineR.GetPosition(0);
+        Vector3 rightEdge = lineR.GetPosition(3);
+
+        //positionen där den kommer stanna utanför skärmen.
+        leftDestination = new Vector2(leftEdge.x - 1f, transform.position.y);
+        rightDestination = new Vector2(rightEdge.x + 1f, transform.position.y);
+
+        SetInvisible();
+    }
+
+
+    void Update()
+    {
+        if (!isVisible) //är den inte synlig så ska den ej röra sig.
+        {
+            return;
+        }
+
+        if (direction == 1)
+        {
+            //rör sig åt höger
+            transform.position += speed * Time.deltaTime * Vector3.right;
+
+            if (transform.position.x >= rightDestination.x)
+            {
+                SetInvisible();
+            }
+        }
+        else
+        {
+            //rör sig åt vänster
+            transform.position += speed * Time.deltaTime * Vector3.left;
+
+            if (transform.position.x <= leftDestination.x)
+            {
+                SetInvisible();
+            }
+        }
+    }
+
+
+    //flyttar den till en plast precis utanför scenen.
+    void SetInvisible()
+    {
+        isVisible = false;
+
+        if (direction == 1)
+        {
+            transform.position = rightDestination;
+        }
+        else
+        {
+            transform.position = leftDestination;
+        }
+
+        Invoke(nameof(SetVisible), cycleTime); //anropar SetVisible efter ett visst antal sekunder
+    }
+
+    void SetVisible()
+    {
+        direction *= -1; //Ändrar riktningen
+
+        isVisible = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Laser"))
+        {
+            SetInvisible();
+            GameManagerKopia.Instance.OnMysteryShipKilled(this);
+        }
+    }
+}

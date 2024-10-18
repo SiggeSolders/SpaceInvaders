992b0e9148aeed7a07e925bc8ef97fc75fecb561
MiraSkold <mira.skold@elev.ga.lbs.se>
2024-10-18 09:25:04 +0200

    Background and main menu
    Bakgrund och meny

---
+++ Assets/Skript/Scenechange.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechange : MonoBehaviour
+{
+    public string scene;
+    public void Play()
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
+

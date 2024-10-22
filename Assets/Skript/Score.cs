b69be5de68c346a0d761524b4ff6889c57ebcb74
MiraSkold <mira.skold@elev.ga.lbs.se>
2024-10-22 10:58:21 +0200

    summary

---
+++ Assets/Skript/Score.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D myRigidbody;
+    public int Objectvalue = 1;
+    public ScoreManager sm;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Invader")
+        {
+            ScorePickup inventory = other.GetComponent<ScorePickup>();
+
+            if (inventory != null)
+            {
+                inventory.objects = inventory.objects + Objectvalue;
+               
+                sm.ScoreCount++;
+            }
+        }
+    }
+}

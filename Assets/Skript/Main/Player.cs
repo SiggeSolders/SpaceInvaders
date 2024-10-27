381872afeacaef081527ec29a744635ab55c2484
CharlieSU23B <charlie.buhrehagglund@elev.ga.lbs.se>
2024-10-27 15:00:02 +0100

    Fixed shooting timing

--- Assets/Skript/Main/Player.cs
+++ Assets/Skript/Main/Player.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
+    GameObject player;
+    AudioSource missileSound;
+
     public LineRenderer lineRendererTarget;
     LineRenderer lineR;
 
@@ -21,6 +24,9 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        player = GameObject.Find("Player");
+        missileSound = player.GetComponent<AudioSource>();
+
         lineR = lineRendererTarget.GetComponent<LineRenderer>();
 
         checkBeatCounter = beatConductor.GetComponent<BeatCounter>();
@@ -51,6 +57,7 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && checkBeatCounter.inSync == true && checkBeatCounter.missleShot <= 0)
         {
             checkBeatCounter.missleShot = 2;
+            missileSound.Play();
             missile = Instantiate(missilePrefab, transform.position, Quaternion.identity);
             //Lägger till en multiplier som används på score-systemet
             if(multiplier < 16)
